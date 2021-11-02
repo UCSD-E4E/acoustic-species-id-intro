a5302980af56ee46844573e5f248b52e54cb670a
Shasanka <nkshasanka@gmail.com>
2021-11-02 16:41:08 -0700

    added

---
+++ sortmoth.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+import csv
+
+df_credit = pd.read_csv ('Peru_2019_AudioMoth_Data_Full.csv')
+
+strata1 = 0;
+strata2 =  0; 
+
+# audioMothID = 'AudioMothID'
+# for typ in list(df_credit[audioMothID].dropna().unique()):
+# 	print(typ, end=' -')
+# 	print(df_credit[df_credit[audioMothID] == typ].sample(3))
+
+print(df_credit)
+	
+
