07bbb78648b5d19b7c21c9a2fab382b23d598f10
Shasanka <snudurupati@ucsd.edu>
2021-12-19 21:34:11 -0800

    Here is the change

--- sortmoth.py
+++ sortmoth.py
@@ -2,16 +2,38 @@ import pandas as pd
 import numpy as np
 import csv
 
-df_credit = pd.read_csv ('Peru_2019_AudioMoth_Data_Full.csv')
+#  -------------------
+# Write a finction that will take in a CSV path and output a new CSV file
+# with a stratified random dample of the original csv file. 
+# Should return T/F ig the stratified csv file was successful. 
+#
+# The CSV file should have #successful Audiomoths * 24 clips. Each clip 
+# should represent a different hour of the day. 
+#     * Make sure the clips chosen are a minute long ( successful regordings)
+#     * They are 46.1 megabytes for reference
 
-strata1 = 0;
-strata2 =  0; 
 
-# audioMothID = 'AudioMothID'
-# for typ in list(df_credit[audioMothID].dropna().unique()):
-# 	print(typ, end=' -')
-# 	print(df_credit[df_credit[audioMothID] == typ].sample(3))
+# The first strata layer to be represented by each Audiomoth device
+#  -  Make it so that IF audiomoth device has enough clips, it will be added to first strata
+#  -  Audiomoth 21, 19, 8, 28 had problems
 
-print(df_credit)
+# Second strata layer to be represented by the hours in the day 
+#     -  The CSV is in UTC , ( 0- 23)
+#     - Approach: You can use pandas to find all the rows in the csv that are in a certain 
+#                 hour, that are a minute long, and put those in a list. You can then randomly select
+#                 a clip from that list. 
+#  -------------------
+
+def strataSort():
+	df = pd.read_csv('Peru_2019_AudioMoth_Data_Full.csv')
+	# Work on First Strata... 
+	# Since there are close to 2852 clips from each device, we need to get a strata sample of 
+	g = df[df.AudioMothCode != "AM-8"]
+	column_csv = g.groupby('AudioMothCode',group_keys=False).apply(lambda x: x.sample())
 	
 
+	# Work on Second Strata... 
+
+	print(column_csv)
+
+strataSort()
